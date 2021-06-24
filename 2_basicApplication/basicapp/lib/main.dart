4934dd9188974e3bbc14f55d87a0ff0ee90613a6
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2021-06-25 04:19:47 +0530

    build() : Network main application can add images

--- 2_basicApplication/basicapp/lib/main.dart
+++ 2_basicApplication/basicapp/lib/main.dart
@@ -1,3 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Home"),
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'),
+            ),
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
